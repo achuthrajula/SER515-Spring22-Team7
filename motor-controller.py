600d1025da0cb66aaa1cfcc9e0fd846f22ecaea0
Varshik <sontivarshik@gmail.com>
2022-02-18 02:18:25 -0700

    added speed control for the rover

--- motor-controller.py
+++ motor-controller.py
@@ -14,15 +14,30 @@ while input_command != "7":
         7. To exit\n"""
     )
 
+
     if input_command == "1":
         os.system(
             "gazebo --verbose /opt/ros/foxy/share/gazebo_plugins/worlds/gazebo_ros_diff_drive_demo.world")
     elif input_command == "2":
         os.system(
             "ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{linear: {x: 1.0}}' -1")
+        speed_input = 1
+        while speed_input != 0:
+                speed_input = float(input("Pick a value from 1 to 9 to set the speed and enter 0 to exit loop \n"))
+                if (speed_input >= 1.0 and speed_input <= 9.0):
+                    value = f'{{linear: {{x: {speed_input}}}}}'
+                    os.system(
+                    f"ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{value}' -1")
     elif input_command == "3":
         os.system(
             "ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{linear: {x: -1.0}}' -1")
+        speed_input = 1
+        while speed_input != 0:
+                speed_input = float(input("Pick a value from -1 to -9 to set the speed and enter 0 to exit loop \n"))
+                if (speed_input >= -9.0 and speed_input <= -1.0):
+                    value = f'{{linear: {{x: {speed_input}}}}}'
+                    os.system(
+                    f"ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{value}' -1")
     elif input_command == "4":
         os.system(
             "ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{linear: {x: 0.0}}' -1")
