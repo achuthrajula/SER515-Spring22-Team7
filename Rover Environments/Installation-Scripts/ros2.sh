cc16b90aade01ea3ba588a8c8cea61e98ca8dcd5
pbumadi <pbumadi@asu.edu>
2022-02-28 18:20:10 -0800

    US-49-Task-52:Ros2 install script

---
+++ Rover Environments/Installation-Scripts/ros2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Beginning ROS 2 Installation"
+
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+echo "Setting up sources"
+
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+echo "Installing ROS2 packages"
+
+sudo apt update
+sudo apt install ros-foxy-desktop
+sudo apt install ros-foxy-ros-base
+
+ 
+echo "Setting up Environment"
+
+source /opt/ros/foxy/setup.bash
