111baa77c011ecac097d6189f900b7be304c0f53
kashrajiv <rajiv97j@gmail.com>
2022-03-18 11:21:57 -0700

    US-62 Task#63 Added logic for configurable sensors

--- ezRos/test_environment/test_drive.py
+++ ezRos/test_environment/test_drive.py
@@ -8,6 +8,17 @@ class TestDrive:
 
     def launch(self):
         wheel_input = input('Enter the number of wheels for the rover\n')
+
+        sensor_input = input(
+            """
+            Choose the sensors you want mounted: \n
+            0. No Sensor
+            1. LASER
+            """
+        )
+        
+
         sys.path.append(os.getcwd())
 
-        os.system(f"gazebo --verbose {os.getcwd()}/Gazebo-Worlds/{wheel_input}x_alpha_gazebo_environment.world")
+        os.system(f"gazebo --verbose {os.getcwd()}/Gazebo-Worlds/{wheel_input}_{sensor_input}_x_alpha_gazebo_environment.world")
+        #os.system(f"gazebo --verbose {os.getcwd()}/Gazebo-Worlds/{wheel_input}x_alpha_gazebo_environment.world")
