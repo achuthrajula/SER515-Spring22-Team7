eb7398230526c13b2563a7ae0008beb2c55844dd
shivani6621 <pshivani387@gmail.com>
2022-04-14 07:38:49 -0700

    Refactoring the chassis logic
    Co-authored-by: shivani6621 <pshivani387@gmail.com>
    Co-authored-by: Varshik Sonti <sontivarshik@gmail.com>

--- ezRos/modular_assembly/modular_chassis.py
+++ ezRos/modular_assembly/modular_chassis.py
@@ -1,101 +1,54 @@
-chassis_generator_8x = """<link name='chassis'>
-        <pose>-0.2 -0 0.2 0 -0 0</pose>
-        <inertial>
-          <mass>1.14395</mass>
-          <inertia>
-            <ixx>0.126164</ixx>
-            <ixy>0</ixy>
-            <ixz>0</ixz>
-            <iyy>0.416519</iyy>
-            <iyz>0</iyz>
-            <izz>0.481014</izz>
-          </inertia>
-        </inertial>
-        <visual name='visual'>
-          <geometry>
-            <mesh>
-              <uri>model://pioneer3at/meshes/chassis.dae</uri>
-              <scale>14 4 4</scale>
-              <pose>0 0 7</pose>
-            </mesh>
-          </geometry>
-        </visual>
-        <collision name='collision'>
-          <geometry>
-            <box>
-              <size>1.01142 1 0.568726</size>
-            </box>
-          </geometry>
-        </collision>
-      </link>"""
-
-chassis_generator_6x = """<link name='chassis'>
-        <pose>-0.2 -0 0.2 0 -0 0</pose>
-        <inertial>
-          <mass>1.14395</mass>
-          <inertia>
-            <ixx>0.126164</ixx>
-            <ixy>0</ixy>
-            <ixz>0</ixz>
-            <iyy>0.416519</iyy>
-            <iyz>0</iyz>
-            <izz>0.481014</izz>
-          </inertia>
-        </inertial>
-        <visual name='visual'>
-          <geometry>
-            <mesh>
-              <uri>model://pioneer3at/meshes/chassis.dae</uri>
-              <scale>12 4 4</scale>
-              <pose>0 0 7</pose>
-            </mesh>
-          </geometry>
-        </visual>
-        <collision name='collision'>
-          <geometry>
-            <box>
-              <size>1.01142 1 0.568726</size>
-            </box>
-          </geometry>
-        </collision>
-      </link>"""
-
-chassis_generator_4x = """<link name='chassis'>
-        <pose>-0.2 -0 0.2 0 -0 0</pose>
-        <inertial>
-          <mass>1.14395</mass>
-          <inertia>
-            <ixx>0.126164</ixx>
-            <ixy>0</ixy>
-            <ixz>0</ixz>
-            <iyy>0.416519</iyy>
-            <iyz>0</iyz>
-            <izz>0.481014</izz>
-          </inertia>
-        </inertial>
-        <visual name='visual'>
-          <geometry>
-            <mesh>
-              <uri>model://pioneer3at/meshes/chassis.dae</uri>
-              <scale>4 4 4</scale>
-              <pose>0 0 7</pose>
-            </mesh>
-          </geometry>
-        </visual>
-        <collision name='collision'>
-          <geometry>
-            <box>
-              <size>2.01142 1 0.568726</size>
-            </box>
-          </geometry>
-        </collision>
-      </link>"""
-
+chassis_generator = {
+  '2x_4x': {
+    'scale': '4 4 4',
+    'size': '2.01142 1 0.568726',
+  },
+  '6x': {
+    'scale': '12 4 4',
+    'size': '1.01142 1 0.568726',
+  },
+  '8x': {
+    'scale': '11 4 4',
+    'size': '1.01142 1 0.568726',
+  },
+}
 
 def generate_chassis(num_wheels):
     if num_wheels == '2' or num_wheels == '4':
-        return chassis_generator_4x
+        value = chassis_generator['2x_4x']
     elif num_wheels == '6':
-        return chassis_generator_6x
+        value = chassis_generator['6x']
     elif num_wheels == '8':
-        return chassis_generator_8x
+        value = chassis_generator['8x']
+
+    chassis = f"""<link name='chassis'>
+        <pose>-0.2 -0 0.2 0 -0 0</pose>
+        <inertial>
+          <mass>1.14395</mass>
+          <inertia>
+            <ixx>0.126164</ixx>
+            <ixy>0</ixy>
+            <ixz>0</ixz>
+            <iyy>0.416519</iyy>
+            <iyz>0</iyz>
+            <izz>0.481014</izz>
+          </inertia>
+        </inertial>
+        <visual name='visual'>
+          <geometry>
+            <mesh>
+              <uri>model://pioneer3at/meshes/chassis.dae</uri>
+              <scale>{value['scale']}</scale>
+              <pose>0 0 7</pose>
+            </mesh>
+          </geometry>
+        </visual>
+        <collision name='collision'>
+          <geometry>
+            <box>
+              <size>{value['size']}</size>
+            </box>
+          </geometry>
+        </collision>
+      </link>"""
+    return chassis
