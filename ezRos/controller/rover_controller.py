6ce2b4db3bca79149fe2ab2de9826d7aa6ef0d9f
Varshik Sonti <sontivarshik@gmail.com>
2022-04-15 19:02:12 -0700

    Using teleop to control the rover

--- ezRos/controller/rover_controller.py
+++ ezRos/controller/rover_controller.py
@@ -1,67 +1,107 @@
-import os
+import sys
+import geometry_msgs.msg
+import rclpy, termios, tty
 
 class Controller:
 
     def __init__(self) -> None:
         pass
+   
+    def getKey(self,settings):
+        tty.setraw(sys.stdin.fileno())
+        key = sys.stdin.read(1)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        return key
 
     def controller(self):
-        input_command = 0
+        msg = """
+        Use the following commands to control the rover
+        ---------------------------
+        Moving around:         
+                            w, a ,s ,d to control the rover linearly
+        q    w    e         q, e to increase and decrease the linear velocity of the rover              
+        a    s    d         z, v to move the rover angularly
+        z    x    v         x to stop the rover
+                            
+        CTRL-C to quit
+        """
 
-        while input_command != "x":
-            input_command = input(
-                """Commands: \n
-                K to start the rover
-                Use the following numbers to control the rover: \n
-                                Q & E to (+/-) velocity
-                   W            C to exit velocity control
-                A  S  D         R to stop rover
-                                X to exit program
-                """
-            )
+        moveBindings = {
+            'w': (1, 0, 0, 0),
+            'a': (1, 0, 0, 1),
+            'd': (1, 0, 0, -1),
+            's': (-1, 0, 0, 0),
+            'j': (0, 0, 0, 1),
+            'l': (0, 0, 0, -1),
+            'z': (-1, 0, 0, 1),
+            'v': (-1, 0, 0, -1),
+        }
 
-            if input_command == "w":
-                os.system(
-                    "ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{linear: {x: 1.0}}' -1")
-                speed_input = 1
-                user_input = ''
-                while user_input != 'c':
-                    user_input = input()
-                    if (user_input == "q"):
-                        speed_input += 1
-                        value = f'{{linear: {{x: {speed_input}}}}}'
-                        os.system(
-                            f"ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{value}' -1")
-                    elif (user_input == "e"):
-                        speed_input -= 1
-                        value = f'{{linear: {{x: {speed_input}}}}}'
-                        os.system(
-                            f"ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{value}' -1")
-            elif input_command == "s":
-                os.system(
-                    "ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{linear: {x: -1.0}}' -1")
-                speed_input = 1
-                user_input = ''
-                while user_input != 'c':
-                    user_input = input()
-                    if (user_input == "q"):
-                        speed_input += 1
-                        value = f'{{linear: {{x: -{speed_input}}}}}'
-                        os.system(
-                            f"ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{value}' -1")
-                    elif (user_input == "e"):
-                        speed_input -= 1
-                        value = f'{{linear: {{x: -{speed_input}}}}}'
-                        os.system(
-                            f"ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{value}' -1")
-            elif input_command == "r":
-                os.system(
-                    "ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{linear: {x: 0.0}}' -1")
-            elif input_command == "a":
-                os.system(
-                    "ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{angular: {z: 1.0}}' -1")
-            elif input_command == "d":
-                os.system(
-                    "ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{angular: {z: -1.0}}' -1")
-            elif input_command == "x":
-                return
+        speedBindings = {
+            'q': (1.1, 1),
+            'e': (.9, 1),
+        }
+
+        settings = termios.tcgetattr(sys.stdin)
+
+        rclpy.init()
+        node = rclpy.create_node('teleop_twist_keyboard')
+        pub = node.create_publisher(geometry_msgs.msg.Twist, '/demo/cmd_demo', 10)
+
+        speed = 1.0
+        turn = 1.0
+        x = 0.0
+        y = 0.0
+        z = 0.0
+        th = 0.0
+        status = 0.0
+
+        try:
+            print(msg)
+            print('current speed: %s' % (speed))
+            while True:
+                key = self.getKey(settings)
+                if key in moveBindings.keys():
+                    x = moveBindings[key][0]
+                    y = moveBindings[key][1]
+                    z = moveBindings[key][2]
+                    th = moveBindings[key][3]
+                elif key in speedBindings.keys():
+                    speed = speed * speedBindings[key][0]
+                    turn = turn * speedBindings[key][1]
+                    print('current speed: %s' % (speed))
+                    if (status == 14):
+                        print(msg)
+                    status = (status + 1) % 15
+                elif key == 'x':
+                    x = 0.0
+                    y = 0.0
+                    z = 0.0
+                    th = 0.0
+                elif (key == '\x03'):
+                        break
+
+                twist = geometry_msgs.msg.Twist()
+                twist.linear.x = x * speed
+                twist.linear.y = y * speed
+                twist.linear.z = z * speed
+                twist.angular.x = 0.0
+                twist.angular.y = 0.0
+                twist.angular.z = th * turn
+                pub.publish(twist)
+
+        except Exception as e:
+            print(e)
+
+        finally:
+            twist = geometry_msgs.msg.Twist()
+            twist.linear.x = 0.0
+            twist.linear.y = 0.0
+            twist.linear.z = 0.0
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z = 0.0
+            pub.publish(twist)
+
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+            
